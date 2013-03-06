273f7bc12e3d8e72955e94034f2c0d6479c93c0e
Nathan Fiedler <nathan.j.fiedler@gmail.com>
2013-03-06 09:48:02 -0600

    Changed default admin account to admin@example.com

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 // You may want to change these variable values for your project
-var adminEmail = "admin@domain.com";
+var adminEmail = "admin@example.com";
 var adminPassword = "123456";
 var numberOfProjectsToCreateOnFirstStartup = 3;          // Set to 0 for no sample data
 var numberOfIssuesToCreatePerProjectOnFirstStartup = 20; // Set to 0 for no sample data
